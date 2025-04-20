3053e378bd865210765ccdc846a825fe97351cd9
Reema <Reemaalrabia@gmail.com>
2025-04-21 00:58:04 +0300

    أضفت مسار استقبال الصوت من السيرفر الثاني

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 async def upload_audio(file: UploadFile = File(...)):
     file_location = f"{UPLOAD_DIR}/{file.filename}"
     
-    # حفظ الملف
+    # حفظ الملف الصوتي
     with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # إرسال بيانات تجريبية للسيرفر الأول
+    # إرسال بيانات تجريبية للسيرفر الأول (تعديل لاحقًا)
     payload = {
         "time": 45.0,
         "sound_level": 0.22
@@ -25,15 +25,16 @@ async def upload_audio(file: UploadFile = File(...)):
 
     try:
         response = requests.post(
-            "https://رابط-السيرفر-الأول.onrender.com/predict",  # غيّري الرابط هنا
+            "https://echo-api-ceeq.onrender.com/predict",
             json=payload
         )
         prediction_result = response.json()
+        message = "تم توصيل تشجيعك بنجاح!"
     except Exception as e:
         prediction_result = {"error": "تعذر الاتصال بالسيرفر الأول", "details": str(e)}
+        message = "تم رفع الصوت لكن لم يتم توصيله للملعب"
 
     return JSONResponse(content={
-        "message": "تم رفع الملف بنجاح",
-        "filename": file.filename,
+        "message": message,
         "prediction": prediction_result
     })
